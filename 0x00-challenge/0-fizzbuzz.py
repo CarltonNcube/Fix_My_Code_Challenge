209f31f3555df3080f388d53ca4a4af042cd57ec
CarltonNcube <carltonncube@outlook.com>
2023-10-29 14:02:32 -0700

    my implementation of FizzBuzz in Python

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -15,12 +15,14 @@ def fizzbuzz(n):
         return
 
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            print("FizzBuzz")
-        elif (i % 3) == 0:
-            print("Fizz")
-        elif (i % 5) == 0:
-            print("Buzz")
+        output = ""
+        if (i % 3) == 0:
+            output += "Fizz"
+        if (i % 5) == 0:
+            output += "Buzz"
+
+        if output:
+            print(output)
         else:
             print(i)
 
