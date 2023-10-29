758dccaacae5fa9ef5ceda363818f05d80da5ffd
CarltonNcube <carltonncube@outlook.com>
2023-10-29 13:50:44 -0700

    my implementation of FizzBuzz in Python

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
+import sys
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three, print "Fizz" instead of the number.
+    - For multiples of five, print "Buzz" instead of the number.
+    - For numbers which are multiples of both three and five, print "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            print("FizzBuzz")
+        elif (i % 3) == 0:
+            print("Fizz")
+        elif (i % 5) == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == '__main':
+    if len(sys.argv) != 2:
+        print("Usage: ./0-fizzbuzz.py <number>")
+        sys.exit(1)
+
+    try:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
+    except ValueError:
+        print("Invalid input. Please provide a valid integer.")
